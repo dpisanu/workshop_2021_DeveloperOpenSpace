fe797096083e975d0150508fee825b2490e3f3bf
Daniel P <dpisanu@users.noreply.github.com>
2021-11-22 00:24:09 +0100

    base work for dynmic buttons list

--- src/SampleWpfApp/MainWindowViewModel.cs
+++ src/SampleWpfApp/MainWindowViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using CommonControls.ViewModels;
 using Plugin;
 
@@ -14,6 +9,7 @@ namespace SampleWpfApp
         public MainWindowViewModel()
         {
             Plugins = new ObservableCollection<IPlugin>();
+            // Todo : Load Plugins
         }
 
         public ObservableCollection<IPlugin> Plugins { get; private set; }

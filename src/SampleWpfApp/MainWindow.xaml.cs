9fe1af0f90b322ae998117f0286f8851a909f5f2
punker76 <punkerat76@gmail.com>
2021-11-22 21:39:37 +0100

    Add MahApps to Sample App

--- src/SampleWpfApp/MainWindow.xaml.cs
+++ src/SampleWpfApp/MainWindow.xaml.cs
@@ -14,22 +14,23 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CommonControls.ViewModels;
 using CommonControls.Views;
+using MahApps.Metro.Controls;
 
 namespace SampleWpfApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : IMainWindow
+    public partial class MainWindow : MetroWindow, IMainWindow
     {
         private readonly IViewModel _dataContext;
+
         public MainWindow()
         {
             _dataContext = new MainWindowViewModel();
             DataContext = _dataContext;
 
             InitializeComponent();
-
         }
     }
 }

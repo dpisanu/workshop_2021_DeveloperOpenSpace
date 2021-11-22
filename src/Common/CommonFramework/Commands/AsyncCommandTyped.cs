eb804500b9c3c0cfdb653113bf7733beeee9bac1
Daniel P <dpisanu@users.noreply.github.com>
2021-11-22 22:26:17 +0100

    Splitting up AsyncCommand into individual parts

--- src/Common/CommonFramework/Commands/AsyncCommandTyped.cs
+++ src/Common/CommonFramework/Commands/AsyncCommandTyped.cs
@@ -4,34 +4,6 @@ using System.Windows.Input;
 
 namespace Common.CommonFramework.Commands
 {
-    public interface IErrorHandler
-    {
-        void HandleError(Exception ex);
-    }
-
-    public static class TaskUtilities
-    {
-#pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
-        public static async void FireAndForgetSafeAsync(this Task task, IErrorHandler handler = null)
-#pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
-        {
-            try
-            {
-                await task;
-            }
-            catch (Exception ex)
-            {
-                handler?.HandleError(ex);
-            }
-        }
-    }
-
-    public interface IAsyncCommand<T> : ICommand
-    {
-        Task ExecuteAsync(T parameter);
-        bool CanExecute(T parameter);
-    }
-
     public class AsyncCommand<T> : IAsyncCommand<T>
     {
         public event EventHandler CanExecuteChanged;
---
+++ src/Common/CommonFramework/Commands/IErrorHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Common.CommonFramework.Commands
+{
+    public interface IErrorHandler
+    {
+        void HandleError(Exception ex);
+    }
+}

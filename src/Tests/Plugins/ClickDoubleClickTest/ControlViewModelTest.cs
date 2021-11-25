7415701f443811a7d9a262187597e6c063844303
Daniel P <dpisanu@users.noreply.github.com>
2021-11-25 16:22:38 +0100

    Adding more test content

--- src/Tests/Plugins/ClickDoubleClickTest/ControlViewModelTest.cs
+++ src/Tests/Plugins/ClickDoubleClickTest/ControlViewModelTest.cs
@@ -14,5 +14,49 @@ namespace ClickDoubleClickTest
         {
             Assert.DoesNotThrow(() => new Plugins.ClickDoubleClick.ControlViewModel());
         }
+
+        [Test]
+        public void DoubleclickCommand()
+        {
+            var vm = new Plugins.ClickDoubleClick.ControlViewModel();
+            var color = vm.FieldOneColor;
+            vm.ButtonOneClick.Execute(null);
+            Assert.That(vm.FieldOneColor, Is.Not.EqualTo(color));
+            vm.ButtonOneClick.Execute(null);
+            Assert.That(vm.FieldOneColor, Is.EqualTo(color));
+        }
+
+        [Test]
+        public void ButtonOneDoubleClickCommand()
+        {
+            var vm = new Plugins.ClickDoubleClick.ControlViewModel();
+            var color = vm.FieldTwoColor;
+            vm.ButtonOneDoubleClick.Execute(null);
+            Assert.That(vm.FieldTwoColor, Is.Not.EqualTo(color));
+            vm.ButtonOneDoubleClick.Execute(null);
+            Assert.That(vm.FieldTwoColor, Is.EqualTo(color));
+        }
+
+        [Test]
+        public void ButtonTwoClickCommand()
+        {
+            var vm = new Plugins.ClickDoubleClick.ControlViewModel();
+            var color = vm.FieldTwoColor;
+            vm.ButtonTwoClick.Execute(null);
+            Assert.That(vm.FieldThreeColor, Is.Not.EqualTo(color));
+            vm.ButtonTwoClick.Execute(null);
+            Assert.That(vm.FieldThreeColor, Is.EqualTo(color));
+        }
+
+        [Test]
+        public void ButtonTwoDoubleClickCommand()
+        {
+            var vm = new Plugins.ClickDoubleClick.ControlViewModel();
+            var color = vm.FieldFourColor;
+            vm.ButtonTwoDoubleClick.Execute(null);
+            Assert.That(vm.FieldFourColor, Is.Not.EqualTo(color));
+            vm.ButtonTwoDoubleClick.Execute(null);
+            Assert.That(vm.FieldFourColor, Is.EqualTo(color));
+        }
     }
 }
--- src/Tests/Plugins/ListBoxTest/ControlViewModelTest.cs
+++ src/Tests/Plugins/ListBoxTest/ControlViewModelTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Linq;
 
 namespace ListBoxTest
 {
@@ -13,6 +14,44 @@ namespace ListBoxTest
         public void Ctor()
         {
             Assert.DoesNotThrow(() => new Plugins.ListBox.ControlViewModel());
+        }
+
+        [TestCase("random")]
+        public void TextBoxValue(string value)
+        {
+            var vm = new Plugins.ListBox.ControlViewModel();
+            var textBoxValue = vm.TextBoxValue;
+            Assert.That(vm.TextBoxValue, Is.Not.EqualTo(value));
+            vm.TextBoxValue = value;
+            Assert.That(vm.TextBoxValue, Is.EqualTo(value));
+        }
+
+        [Test]
+        public void ComboBoxContent()
+        {
+            var vm = new Plugins.ListBox.ControlViewModel();
+            Assert.That(vm.ComboBoxContent.Any());
+        }
+
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("random", true)]
+        public void AddCommandCanExecute(string value, bool canExecute)
+        {
+            var vm = new Plugins.ListBox.ControlViewModel();
+            vm.TextBoxValue = value;
+            Assert.That(vm.AddCommand.CanExecute(null), Is.EqualTo(canExecute));
+        }
+
+
+        [TestCase("random")]
+        public void AddCommandExecute(string value)
+        {
+            var vm = new Plugins.ListBox.ControlViewModel();
+            Assert.That(vm.ComboBoxContent.Contains(value), Is.False);
+            vm.TextBoxValue = value;
+            vm.AddCommand.Execute(null);
+            Assert.That(vm.ComboBoxContent.Contains(value), Is.True);
         }
     }
 }
